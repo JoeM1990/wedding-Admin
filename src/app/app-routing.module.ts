ab8ea1e112ad46b93d9aeb9166b9ce00bb59bbe0
Joe Monkila <jnkiwa25@gmail.com>
2023-02-15 10:17:01 +0100

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: 'login', component:LoginComponent},
+  { path: 'dashboard', component:DashboardComponent},
 ];
 
 @NgModule({
