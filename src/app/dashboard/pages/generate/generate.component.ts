0b1d1363bb40f76db2deecb810b78c3c495facc3
Joe Monkila <jnkiwa25@gmail.com>
2023-02-16 10:39:28 +0100

    commit

--- src/app/dashboard/pages/generate/generate.component.ts
+++ src/app/dashboard/pages/generate/generate.component.ts
@@ -8,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class GenerateComponent implements OnInit {
 
   title1:any;
-  
+  title2:any;
+  title3:any;
+  title4:any;
+
   constructor() { }
 
   ngOnInit(): void {
