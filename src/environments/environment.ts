011541541f2f802cbbc595dd63089ae6169b48c4
Joe Monkila <jnkiwa25@gmail.com>
2023-02-17 12:34:11 +0100

    adding auth config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyBQMLwt9s7Tb1EyzulNaNC-iExpJ4Velzc",
+    authDomain: "wedding-app-96b3b.firebaseapp.com",
+    projectId: "wedding-app-96b3b",
+    storageBucket: "wedding-app-96b3b.appspot.com",
+    messagingSenderId: "474896962980",
+    appId: "1:474896962980:web:cdf0f565d64d0954f11d31",
+    measurementId: "G-VQCJ9E1LM3"
+  }
 };
 
 /*
