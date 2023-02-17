2163618347a1ce47a9ea6e4d0f8cd179719db247
Joe Monkila <jnkiwa25@gmail.com>
2023-02-17 15:09:58 +0100

    customs dialog

--- src/app/remote/auth/auth.service.ts
+++ src/app/remote/auth/auth.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { ErrorComponent } from 'src/app/dialog/error/error.component';
+import { SuccessComponent } from 'src/app/dialog/success/success.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(public fireauth : AngularFireAuth, public router: Router) { }
+  constructor(public fireauth : AngularFireAuth, public router: Router,public dialog:MatDialog) { }
 
   login(email : string, password : string){
     this.fireauth.signInWithEmailAndPassword(email,password).then( () =>{
@@ -17,13 +20,13 @@ export class AuthService {
     }, err => {
         const timeout=1200;
 
-        // let dialogRef=this.dialog.open(ErrorComponent,{data:'Nom d utilisateur ou mot de passe incorrect'});
+        let dialogRef=this.dialog.open(ErrorComponent,{data:'Nom d utilisateur où Mot de passe incorrect'});
 
-        // dialogRef.afterOpened().subscribe(_ => {
-        //   setTimeout(() => {
-        //      dialogRef.close();
-        //   }, timeout)
-        // })
+        dialogRef.afterOpened().subscribe(_ => {
+          setTimeout(() => {
+             dialogRef.close();
+          }, timeout)
+        })
 
        //alert('Erreur de connexion');
 
@@ -35,10 +38,33 @@ export class AuthService {
 
   register(email: string, password: string){
     this.fireauth.createUserWithEmailAndPassword(email,password).then( () => {
-      alert('Enregistrement effectue')
+
+      const timeout=1200;
+
+      let dialogRef=this.dialog.open(SuccessComponent,{data:'Enregistrement effectué avec succes'});
+
+        dialogRef.afterOpened().subscribe(_ => {
+          setTimeout(() => {
+             dialogRef.close();
+          }, timeout)
+        })
+
+
+      //alert('Enregistrement effectue')
       this.router.navigate(['/login']);
     }, err => {
-      alert(err.message);
+      //alert(err.message);
+
+      const timeout=1200;
+
+        let dialogRef=this.dialog.open(ErrorComponent,{data:'Echec d enregistrement'});
+
+        dialogRef.afterOpened().subscribe(_ => {
+          setTimeout(() => {
+             dialogRef.close();
+          }, timeout)
+        })
+
       this.router.navigate(['/login']);
     }
 
