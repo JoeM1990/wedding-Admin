cbfd6da71eb3b820a36f161a6e9debad428efa61
Joe Monkila <jnkiwa25@gmail.com>
2023-02-17 12:39:02 +0100

    config auth

---
+++ src/app/remote/auth/auth.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(public fireauth : AngularFireAuth, public router: Router) { }
+
+  login(email : string, password : string){
+    this.fireauth.signInWithEmailAndPassword(email,password).then( () =>{
+        localStorage.setItem('token','true');
+        this.router.navigate(['/dashboard']);
+        return true;
+    }, err => {
+        const timeout=1200;
+
+        // let dialogRef=this.dialog.open(ErrorComponent,{data:'Nom d utilisateur ou mot de passe incorrect'});
+
+        // dialogRef.afterOpened().subscribe(_ => {
+        //   setTimeout(() => {
+        //      dialogRef.close();
+        //   }, timeout)
+        // })
+
+        //alert('Erreur de connexion');
+
+        
+        this.router.navigate(['/login']);
+    } ) 
+
+  }
+
+  register(email: string, password: string){
+    this.fireauth.createUserWithEmailAndPassword(email,password).then( () => {
+      alert('Enregistrement effectue')
+      this.router.navigate(['/login']);
+    }, err => {
+      alert(err.message);
+      this.router.navigate(['/register']);
+    }
+
+    )
+
+   
+  }
+
+  logout(){
+    this.fireauth.signOut().then( () => {
+      localStorage.removeItem('token');
+      this.router.navigate(['/login']);
+    }, err  => {
+      alert(err.message);
+    })
+  }
+
+  checkLogin(){
+    return !! localStorage.getItem('token');
+  }
+
+
+}
