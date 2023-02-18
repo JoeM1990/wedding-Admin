0bbfee5d8754a804ef5cc5ba20bd9e2d87f6d209
Joe Monkila <jnkiwa25@gmail.com>
2023-02-18 12:06:27 +0100

    crud invitation

--- src/app/utils/crud/crud.service.ts
+++ src/app/utils/crud/crud.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { finalize } from 'rxjs';
+import { FileUpload } from '../model/file-upload';
+import { Item } from '../model/item';
 
 @Injectable({
   providedIn: 'root'
@@ -13,4 +16,49 @@ export class CrudService {
 
   constructor(private angularFirestore: AngularFirestore,private storage: AngularFireStorage,private db: AngularFireDatabase) { }
 
+
+  addInvitation(fileUpload: FileUpload,item:Item){
+    const filePath = `${this.basePath2}/${fileUpload.file.name}`;
+    const storageRef = this.storage.ref(filePath);
+    const uploadTask = this.storage.upload(filePath, fileUpload.file);
+
+    uploadTask.snapshotChanges().pipe(
+      finalize(() => {
+      storageRef.getDownloadURL().subscribe(downloadURL => {
+      fileUpload.url = downloadURL;
+      fileUpload.name = fileUpload.file.name;
+
+      item.description=downloadURL;
+
+      this.addItem(item);
+      
+      });
+        })
+      ).subscribe();
+     
+  }
+
+  private addItem(item:Item){
+    return new Promise<any>((resolve, reject) => {
+      this.angularFirestore.collection('item-invitation')
+      .add(item)
+      .then(response => { }, error => reject(error))
+      ;
+      })
+  }
+
+  deleteItem(item: { id: string | undefined; }){
+    return this.angularFirestore.collection('item-invitation')
+    .doc(item.id)
+    .delete();
+  }
+
+  getAllItem(){
+    return this.angularFirestore.collection('item-invitation')
+    .snapshotChanges();
+  }
+
+  
+
+  
 }
