1ae20b39635663d8e240a300495f84db0740b1aa
Joe Monkila <jnkiwa25@gmail.com>
2023-02-18 11:40:54 +0100

    update model

---
+++ src/app/utils/model/item-qr.spec.ts
@@ -0,0 +1,7 @@
+import { ItemQr } from './item-qr';
+
+describe('ItemQr', () => {
+  it('should create an instance', () => {
+    expect(new ItemQr()).toBeTruthy();
+  });
+});
---
+++ src/app/utils/model/item-qr.ts
@@ -0,0 +1,5 @@
+export class ItemQr {
+    id:string|undefined;
+    email_user:string|undefined;
+    description:string|undefined;
+}
