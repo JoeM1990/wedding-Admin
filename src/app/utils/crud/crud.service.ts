c3e104e206aec362b4779517bb5d9c9ee2e4a117
Joe Monkila <jnkiwa25@gmail.com>
2023-02-18 11:34:39 +0100

    crud file

--- src/app/utils/crud/crud.service.ts
+++ src/app/utils/crud/crud.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrudService {
 
-  constructor() { }
+  private basePath = '/uploadQr';
+  private basePath2 = '/uploadInvitation';
+
+  constructor(private angularFirestore: AngularFirestore,private storage: AngularFireStorage,private db: AngularFireDatabase) { }
+
 }
