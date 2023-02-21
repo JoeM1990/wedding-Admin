46751c78c3e251d5c570e5051c8f3d73177f9f97
Joe Monkila <jnkiwa25@gmail.com>
2023-02-21 09:00:20 +0100

    update routing app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { CompleAdminComponent } from './dashboard/pages/comple-admin/comple-admin.component';
 import { CompleClientComponent } from './dashboard/pages/comple-client/comple-client.component';
 import { GenerateComponent } from './dashboard/pages/generate/generate.component';
+import { ListViewComponent } from './dashboard/pages/list-view/list-view.component';
 import { PaiementComponent } from './dashboard/pages/paiement/paiement.component';
 import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './utils/guard/auth.guard';
@@ -16,6 +17,7 @@ const routes: Routes = [
   { path: 'compte-clients', component:CompleClientComponent,canActivate:[AuthGuard]},
   { path: 'paiement', component:PaiementComponent,canActivate:[AuthGuard]},
   { path: 'generate', component:GenerateComponent,canActivate:[AuthGuard]},
+  { path: 'listes', component:ListViewComponent,canActivate:[AuthGuard]},
 ];
 
 @NgModule({
