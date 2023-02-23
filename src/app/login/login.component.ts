14fbd685fecef39a2f619700bcce7737404ad2a0
Joe Monkila <jnkiwa25@gmail.com>
2023-02-23 10:24:09 +0100

    update login from api

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -31,7 +31,6 @@ export class LoginComponent implements OnInit {
         }else{
           this.router.navigate(['/login']);
         }
-
         localStorage.setItem('token',token);
         localStorage.setItem('email_user',email);
       },
