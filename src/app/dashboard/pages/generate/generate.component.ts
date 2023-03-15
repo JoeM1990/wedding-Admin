d6093030c4f3c14b61151f271fdcf2b1ff197851
Joe Monkila <jnkiwa25@gmail.com>
2023-03-15 15:06:31 +0100

    update ui generate

--- src/app/dashboard/pages/generate/generate.component.ts
+++ src/app/dashboard/pages/generate/generate.component.ts
@@ -197,7 +197,7 @@ export class GenerateComponent implements OnInit {
       this.currentFileUpload = new FileUpload(file);
       this.crud.addInvitation(this.currentFileUpload,this.infosForm.value);
 
-      window.location.reload()
+      //window.location.reload()
 
     }
     
