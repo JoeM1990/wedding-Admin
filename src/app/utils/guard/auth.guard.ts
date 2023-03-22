f87836660bf90fd6fe309714fcdbf821cca96271
Joe Monkila <jnkiwa25@gmail.com>
2023-03-22 15:13:49 +0100

    update all

--- src/app/utils/guard/auth.guard.ts
+++ src/app/utils/guard/auth.guard.ts
@@ -1,24 +1,39 @@
 import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Router } from '@angular/router';
 
 import { Observable } from 'rxjs';
+import { ErrorComponent } from 'src/app/dialog/error/error.component';
 import { AuthService } from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(public auth:AuthService, public router:Router){}
+  constructor(public auth:AuthService, public router:Router, public dialog:MatDialog){}
 
   canActivate(){
     if(this.auth.checkLogin()){
       return true;
     }
       this.router.navigate(['/login']);
-      alert("Veuillez d'abord vous connecter");
+      this.dialogError("Veuillez d'abord vous connecter");
+      //alert("Veuillez d'abord vous connecter");
     return false;
     
+  }
+
+  dialogError(message:any){
+    const timeout=1200;
+
+        let dialogRef=this.dialog.open(ErrorComponent,{data:message});
+
+        dialogRef.afterOpened().subscribe(_ => {
+          setTimeout(() => {
+             dialogRef.close();
+          }, timeout)
+        })
   }
   
 }
