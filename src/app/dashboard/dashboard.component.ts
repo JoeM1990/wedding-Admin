cc7f41a9485d7d03f69cb94ff3de428529b1ebbc
Joe Monkila <jnkiwa25@gmail.com>
2023-04-01 12:10:04 +0100

    splash screen

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -116,7 +116,7 @@ export class DashboardComponent implements OnInit {
   checkRole(){
     if(this.cookieService.get('role')=='Client'){
       this.checkingRole=false;
-      this.checkingRole3=false;
+      //this.checkingRole3=false;
     }else if(this.cookieService.get('role')=='Admin'){
       this.checkingRole2=false;
       this.checkingRole3=true;
