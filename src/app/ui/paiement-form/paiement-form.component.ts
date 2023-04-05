72beea4e797b697caa1a21459b5481ee081d729d
Joe Monkila <jnkiwa25@gmail.com>
2023-04-05 10:36:21 +0100

    update all

--- src/app/ui/paiement-form/paiement-form.component.ts
+++ src/app/ui/paiement-form/paiement-form.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { ConfirmationComponent } from 'src/app/dialog/confirmation/confirmation.component';
 import { CrudService } from 'src/app/utils/crud/crud.service';
 import  *  as CryptoJS from  'crypto-js';
 import { WaitingComponent } from 'src/app/dialog/waiting/waiting.component';
+import { ErrorComponent } from 'src/app/dialog/error/error.component';
 
 @Component({
   selector: 'app-paiement-form',
@@ -22,8 +23,8 @@ export class PaiementFormComponent implements OnInit {
     this.transactionForm = this.formBuilder.group({
       email: this.getDataEmail('yyyy-0000'),
       forfait:[''],
-      operateur:[''],
-      reference:[''],
+      operateur:['',[Validators.required]],
+      reference:['',[Validators.required, Validators.minLength(9)]],
       status:"NonValide"
     })
   }
@@ -38,38 +39,41 @@ export class PaiementFormComponent implements OnInit {
   }
 
   addTransaction(){
-    let refDialog=this.dialog.open(ConfirmationComponent,{data:'Voulez-vous ajouter cette reference?'});
+
+      let refDialog=this.dialog.open(ConfirmationComponent,{data:'Voulez-vous ajouter cette reference?'});
 
 
-    refDialog.afterClosed().subscribe(res=>{
-      if(res == 'true'){
-
-
-        let dialogRef=this.dialog.open(WaitingComponent);
-
-        dialogRef.afterOpened().subscribe(_ => {
-          setTimeout(() => {
-         dialogRef.close();
-
-         this.crud.addTransaction(this.transactionForm.value)
-        .subscribe(
-          response =>{
-            if(response){
-              //window.location.reload();
+      refDialog.afterClosed().subscribe(res=>{
+        if(res == 'true'){
+  
+  
+          let dialogRef=this.dialog.open(WaitingComponent);
+  
+          dialogRef.afterOpened().subscribe(_ => {
+            setTimeout(() => {
+           dialogRef.close();
+  
+           this.crud.addTransaction(this.transactionForm.value)
+          .subscribe(
+            response =>{
+              if(response){
+                //window.location.reload();
+              }
+            },
+            error =>{
+              //console.log(error['message']);
             }
-          },
-          error =>{
-            console.log(error['message']);
-          }
-        );
-         
-         }, 1700)
-        })
-
-        
-            
-      }
-    })
+          );
+           
+           }, 1700)
+          })
+  
+          
+              
+        }
+      })
+ 
+   
   }
 
   private decryptEmail(txtToDecrypt: string) {
@@ -81,5 +85,17 @@ export class PaiementFormComponent implements OnInit {
     return this.decryptEmail(data);
   }
 
+
+  dialogError(message:any){
+    const timeout=1400;
+
+        let dialogRef=this.dialog.open(ErrorComponent,{data:message});
+
+        dialogRef.afterOpened().subscribe(_ => {
+          setTimeout(() => {
+             dialogRef.close();
+          }, timeout)
+        })
+  }
 
 }
