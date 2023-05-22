58be1809afec7a7a6c539b4f02e95f17cf1a22ee
Joe Monkila <jnkiwa25@gmail.com>
2023-05-22 12:10:35 +0100

    home pub

--- src/app/ui/contact/contact.component.ts
+++ src/app/ui/contact/contact.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
+import { ErrorComponent } from 'src/app/dialog/error/error.component';
+import { SuccessComponent } from 'src/app/dialog/success/success.component';
 
 @Component({
   selector: 'app-contact',
@@ -23,6 +25,39 @@ export class ContactComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+
+  dialogError(message:any){
+    const timeout=2900;
+
+        let dialogRef=this.dialog.open(ErrorComponent,{data:message});
+
+        dialogRef.afterOpened().subscribe(_ => {
+          setTimeout(() => {
+             dialogRef.close();
+          }, timeout)
+        })
+  }
+
+  dialogSuccess(message:any){
+    const timeout=1400;
+
+      let dialogRef=this.dialog.open(SuccessComponent,{data:message});
+
+        dialogRef.afterOpened().subscribe(_ => {
+          setTimeout(() => {
+             dialogRef.close();
+          }, timeout)
+        })
+  }
+
+  sendMessage(){
+    if(this.contactForm.valid){
+      this.dialogSuccess("Success")
+    }else{
+      this.dialogError("Le formulaire est vide ou mal rempli")
+    }
   }
 
 }
