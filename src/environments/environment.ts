cc362a3d4ee2a1bede066963a6dd2d39f61275d9
Joe Monkila <70669743+JoeM1990@users.noreply.github.com>
2024-07-10 14:38:31 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,15 +5,15 @@
 export const environment = {
   production: true,
   firebase : {
-    apiKey: "AIzaSyBQMLwt9s7Tb1EyzulNaNC-iExpJ4Velzc",
-    authDomain: "wedding-app-96b3b.firebaseapp.com",
-    projectId: "wedding-app-96b3b",
-    storageBucket: "wedding-app-96b3b.appspot.com",
-    messagingSenderId: "474896962980",
-    appId: "1:474896962980:web:cdf0f565d64d0954f11d31",
-    measurementId: "G-VQCJ9E1LM3"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   },
-  baseURL : 'https://www.api-weddingapp.monkila-tech.com/api/'
+  baseURL : ''
 };
 
 /*
